6d3ed597f19504594f5276078331a21f05478c11
SpiritForest <dzakhar@adnoc.ae>
2021-05-23 22:49:29 +0300

    fix: issue with using deprecated package 'body-parser'

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = 4000;
 
 
 db.sync();
-app.use(require('body-parser'));
+app.use(express.json());
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
 app.use('/api/game', game);
